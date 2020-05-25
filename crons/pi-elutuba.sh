f1a8d868bc35a7d6c56bd1c633e101bd4417d22c
bobrock <barret.obrock@gmail.com>
2020-05-25 08:44:48 -0500

    Add cron jobs

---
+++ crons/pi-elutuba.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# VARIABLES
+SENSORS=kavalkilu/sensors
+PY3=/usr/bin/python3
+
+# LOG ANALYSIS
+0 */4 * * *     $PY3    $HOME/$SENSORS/log_reader.py
+32 3 20 * *     $PY3    $HOME/$SENSORS/log_remover.py -lvl debug
+# SYS DATA COLLECTION
+*/10 * * * *    $PY3    $HOME/$SENSORS/net/machine_uptime.py
+# ENV DATA COLLECTION
+*/10 * * * *    $PY3    $HOME/$SENSORS/temps/elutuba_temps.py
+*/10 * * * *    $PY3    $HOME/$SENSORS/temps/darksky_weather.py
