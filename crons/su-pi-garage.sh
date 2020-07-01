95443aade265c69d28464f5bca30c66f95947d5a
Bobrock <barret.obrock@gmail.com>
2020-07-01 16:51:07 -0500

    Fix garage temp collection cron

--- crons/su-pi-garage.sh
+++ crons/su-pi-garage.sh
@@ -7,4 +7,4 @@ PY3=/home/pi/venvs/pitools/bin/python3
 # NIGHTLY REBOOT @23.57
 57 23 * * * /sbin/shutdown -r now
 # ENV DATA COLLECTION
-*/10 * * * *        $PY3    /home/pi/$SENSORS/temps/porch_temps.py -lvl debug
+*/10 * * * *        $PY3    /home/pi/$SENSORS/temps/garage_temps.py -lvl debug
