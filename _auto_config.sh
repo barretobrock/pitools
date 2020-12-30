50d8c8dd001b68cfed652410d49585221d09a2e0
Bobrock <barret.obrock@gmail.com>
2020-12-30 16:55:28 -0600

    Change config repo dir to leverage differing user names

--- _auto_config.sh
+++ _auto_config.sh
@@ -1,7 +1,7 @@
 # **This is an automatically-constructed file. See config.py for the source.**
 
 REPO_NAME="pitools"
-REPO_DIR="/home/bobrock/extras/pitools"
+REPO_DIR="${HOME}/extras/pitools"
 GIT_URL="git+https://github.com/barretobrock/pitools.git#egg=pitools"
 VENV_PATH="${HOME}/venvs/pitools/bin/python3"
 DEP_LINKS=(
