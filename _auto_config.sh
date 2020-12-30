1f80d428553252a813e35dce077eb45b0ad07a44
Bobrock <barret.obrock@gmail.com>
2020-12-30 14:17:34 -0600

    Continuing work on modernizing update routines

---
+++ _auto_config.sh
@@ -0,0 +1,11 @@
+# **This is an automatically-constructed file. See config.py for the source.**
+
+REPO_NAME="pitools"
+REPO_DIR="/home/bobrock/extras/pitools"
+GIT_URL="git+https://github.com/barretobrock/pitools.git#egg=pitools"
+VENV_PATH="${HOME}/venvs/pitools/bin/python3"
+DEP_LINKS=(
+	"git+https://github.com/barretobrock/kavalkilu.git#egg=kavalkilu"
+	"git+https://github.com/barretobrock/easylogger.git#egg=easylogger"
+)
+MAIN_BRANCH="master"
